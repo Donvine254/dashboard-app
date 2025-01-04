5f52a04182c847bc94da3f7c81a1469e6cbc817a
Donvine <donvinemugendi@gmail.com>
2025-01-05 02:44:06 +0300

    Refactor dashboard to use StatCards component for displaying metrics; enhance navigation item styling

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import StatCards from "@/components/stat-cards";
 import { AppSidebar } from "../../components/app-sidebar";
 import { Separator } from "@/components/ui/separator";
 import {
@@ -5,7 +6,6 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { TrendingDown, TrendingUp } from "lucide-react";
 
 export default function Page() {
   return (
@@ -14,7 +14,7 @@ export default function Page() {
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+            <SidebarTrigger className="-ml-1" title="collapse menu" />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <div className="flex flex-col gap-1">
               <p className="font-semibold">🚀 Good morning Mary!</p>
@@ -29,50 +29,9 @@ export default function Page() {
             </div>
           </div>
         </header>
+        <hr />
         <div className="space-y-6">
-          <div className="grid gap-4 md:grid-cols-3 p-2 sm:p-4 md:p-6">
-            {/* first card */}
-            <div className="rounded-lg border bg-card p-6">
-              <div className="text-sm font-medium text-muted-foreground flex justify-between gap-2 items-center">
-                <h3 className="font-semibold text-lg">Total Revenue</h3>
-                <span className="bg-green-100 text-green-700 text-xs flex items-center gap-1 font-medium px-2 py-1 rounded-md">
-                  <TrendingUp />
-                  20.1%
-                </span>
-              </div>
-              <div className="my-2 text-2xl md:text-4xl font-bold">
-                $45,231.89
-              </div>
-              <p className="text-xs text-muted-foreground">
-                From Jan 1st - Jul 31st
-              </p>
-            </div>
-            {/* second card */}
-            <div className="rounded-lg border bg-card p-6">
-              <div className="text-sm font-medium text-muted-foreground flex justify-between gap-2 items-center">
-                <h3 className="font-semibold text-lg">Deliveries</h3>
-                <span className="bg-green-100 text-green-700 text-xs flex items-center gap-1 font-medium px-2 py-1 rounded-md">
-                  <TrendingUp />
-                  120.1%
-                </span>
-              </div>
-              <div className="my-2 text-2xl md:text-4xl font-bold">2,350</div>
-              <p className="text-xs text-muted-foreground">
-                From Jan 1st - Jul 31st
-              </p>
-            </div>
-            <div className="rounded-lg border bg-card p-6">
-              <div className="text-sm font-medium text-muted-foreground flex justify-between gap-2 items-center">
-                <h3 className="font-semibold text-lg">Active Users</h3>
-                <span className="bg-red-100 text-red-700 text-xs flex items-center gap-1 font-medium px-2 py-1 rounded-md">
-                  <TrendingDown />
-                  20.1%
-                </span>
-              </div>
-              <div className="my-2 text-2xl md:text-4xl font-bold">64</div>
-              <p className="text-xs text-muted-foreground">Previous 365 days</p>
-            </div>
-          </div>
+          <StatCards />
         </div>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
--- components/nav-items.tsx
+++ components/nav-items.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { type LucideIcon } from "lucide-react";
-
+import { usePathname } from "next/navigation";
 import {
   SidebarGroup,
   SidebarMenu,
@@ -19,14 +19,17 @@ export function NavItems({
     icon: LucideIcon;
   }[];
 }) {
+  const pathname = usePathname();
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
       <SidebarMenu className="space-y-2">
         {items.map((item) => (
-          <SidebarMenuItem
-            key={item.name}
-            className="hover:bg-blue-100 hover:text-blue-600">
-            <SidebarMenuButton asChild>
+          <SidebarMenuItem key={item.name}>
+            <SidebarMenuButton
+              asChild
+              className={`hover:bg-blue-100 hover:text-blue-600 ${
+                pathname === item.url ? "bg-blue-100 text-blue-700" : ""
+              }`}>
               <a href={item.url} title={item.title}>
                 <item.icon />
                 <span>{item.name}</span>
